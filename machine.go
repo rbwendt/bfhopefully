d1fdb3a1d9ff178bccd35a40fa8390a330534219
Ben Wendt <benw@nextopia.com>
2016-01-20 07:19:22 -0500

    Add machine stuff

--- machine.go
+++ machine.go
@@ -1,40 +1,91 @@
 package bfhopefully
 
-func incrementPointerFn() {}
-func decrementPointerFn() {}
-func incrementByteFn() {}
-func decrementByteFn() {}
-func outputByteFn() {}
-func inputByteFn() {}
-func jumpForwardFn() {}
-func jumpBackwardFn() {}
-	
-var IncrementPointerFn = incrementPointerFn
-var DecrementPointerFn = decrementPointerFn
-var IncrementByteFn = incrementByteFn
-var DecrementByteFn = decrementByteFn
-var OutputByteFn = outputByteFn
-var InputByteFn = inputByteFn
-var JumpForwardFn = jumpForwardFn
-var JumpBackwardFn = jumpBackwardFn
+import (
+	"bufio"
+)
 
-var TokenToOperation = map[Token]func()  {
-	IncrementPointer: IncrementPointerFn,
+func (m *Machine) IncrementPointerFn() {
+	if m.Position == len(m.State) - 1 {
+		m.State = append(m.State, byte(0))
+	}
+	m.Position ++
+}
+
+func (m *Machine) DecrementPointerFn() {
+	if m.Position == 0 {
+		m.State = append([]byte{byte(0)}, m.State...)
+	} else {
+		m.Position --
+	}
+}
+
+func (m *Machine) incrementByteFn() {
+	m.State[m.Position]++
+}
+
+func (m *Machine) decrementByteFn() {
+	m.State[m.Position]--
+}
+
+func (m *Machine) outputByteFn() {
+	m.Output = append(m.Output, m.State[m.Position])
+}
+
+func (m *Machine) InputByteFn() {
+	input, _ := m.Input.ReadRune()
+	m.State[m.Position] = byte(input)
+}
+
+func (m *Machine) JumpForwardFn() {
+	if m.State[m.Position] == 0 {
+		parenCount := 1
+		for i := m.Operation; i < len(m.Operations) ; i++ {
+			if m.Operations[i] == JumpBackward {
+				parenCount --
+				if parenCount == 0 {
+					m.Operation = i
+					break;
+				}
+			} else if m.Operations[i] == JumpForward {
+				parenCount ++
+			}
+		}
+	}
+}
+
+func (m *Machine) JumpBackwardFn() {
+	if m.State[m.Position] != 0 {
+		parenCount := 1
+		for i := m.Operation; i > 0 ; i-- {
+			if m.Operations[i] == JumpForward {
+				parenCount --
+				if parenCount == 0 {
+					m.Operation = i
+					break;
+				}
+			} else if m.Operations[i] == JumpBackward {
+				parenCount ++
+			}
+		}
+	}
 }
 
 type Machine struct {
 	Position int
+	Operation int
 	State []byte
-	Operations []func()
+	Output []byte
+	Input bufio.Reader
+	Operations []Token
 }
 
 func NewMachine(t []Token) Machine {
 	m := Machine{}
-	for i:= 0; i < len(t); i++ {
-		if t[i] == Other {
-			continue
+	m.State = append(m.State, byte(0))
+	for i := 0; i < len(t); i++ {
+		if t[i] != Other {
+			m.Operations = append(m.Operations, t[i])
 		}
-		m.Operations = append(m.Operations, TokenToOperation[t[i]])
 	}
 	return m
 }
--- machine_test.go
+++ machine_test.go
@@ -16,10 +16,19 @@ func TestNewMachine(t *testing.T) {
 	if len(m.Operations) != 1 {
 		t.Error("Unexpected Length")
 	}
+	if m.Operations[0] != b.IncrementPointer {
+		t.Error("Unexpected Operation")
+	}
 
 	m = b.NewMachine([]b.Token{b.DecrementPointer, b.InputByte})
 	if len(m.Operations) != 2 {
 		t.Error("Unexpected Length")
+	}
+	if m.Operations[0] != b.DecrementPointer {
+		t.Error("Unexpected Operation")
+	}
+	if m.Operations[1] != b.InputByte {
+		t.Error("Unexpected Operation")
 	}
 
 }
